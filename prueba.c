e392fa1b82460802c819fb61afaa3eac0f0971ad
UragaMarin <uraga.marin.merid.italibi@gmail.com>
2024-09-20 08:50:40 -0600

    No jala pero tiene sentido (y)

--- prueba.c
+++ prueba.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int altura;
+    int radio;
+    int vol;
+
+    //Scanner
+    printf("Introduce la altura en cm: ");
+    scanf("%d", &altura);
+    printf("Inroduce el radio en cm: ");
+    scanf("%d", &radio);
+    printf("Datos introducidos: ")
+    printf ("Altura introducida : %d \n ", altura);
+
     return 0;
 }
